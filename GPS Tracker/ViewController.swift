6db77de4be1b32771b828dad229a3609e2a61eba
Austin <a.j.baker249@gmail.com>
2025-11-10 17:48:49 +1100

    Initial Commit

---
+++ GPS Tracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GPS Tracker
+//
+//  Created by Austin Baker on 10/11/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GPS TrackerUITests/GPS_TrackerUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  GPS_TrackerUITestsLaunchTests.swift
+//  GPS TrackerUITests
+//
+//  Created by Austin Baker on 10/11/2025.
+//
+
+import XCTest
+
+final class GPS_TrackerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
